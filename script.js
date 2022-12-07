f24324b27a999ef8fcc4c67cfbbed720b56217a0
MindulaHewadewage <mindufernando@gmail.com>
2022-12-07 15:30:44 +0100

    provando

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-console.log ('JS OK')
-
-
 // prendo il documento dalla dom
 const targetElement = document.getElementById('target');
+let numberElement = document.getElementById('number')
 
+// Variabili
+let number = '';
 
 for (let i = 0; i <= 100; i++){
+    number = 'i' ;
     console.log(i);
-    if
+
+    if(i%3==0){
+        number = 'rizz'
+
+    }
+
 }
+
+
+// prendo il documento 
+// faccio la lista di numeri da 1 a 100
+// verifico se un numero è divisibile per 3
+// verifico se un numero è divisibile per 5
+// verifico se un numero è divisibile per entrambi 
