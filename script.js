fcd2314b674763f09ddf0553901285a7fcb74d39
MindulaHewadewage <mindufernando@gmail.com>
2022-12-07 14:15:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log ('JS OK')
+
+
+// prendo il documento dalla dom
+const targetElement = document.getElementById('target');
+
+
+for (let i = 0; i <= 100; i++){
+    console.log(i);
+    if
+}
