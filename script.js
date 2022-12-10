1660b2af0b2cf43af834ab928377c39e4383e2a4
MindulaHewadewage <mindufernando@gmail.com>
2022-12-10 19:36:31 +0100

    struttura

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 // prendo il documento dalla dom
 const targetElement = document.getElementById('target');
 
-// Variabili
+
 
 
 for (let i = 0; i <= 100; i++){
 
     let number = '';
     
-    if(i%3 === 0){
+    if(i%3 == 0 && i%5 == 0){
+    number = 'fizzbuzz';
+    console.log(number);
+
+    }
+    else if(i%3 == 0){
     number = 'fizz';
     console.log(number);
 
     }
-    if(i%5 === 0){
-        number = 'buzz';
-        console.log(number);
+    else if(i%5 == 0){
+    number = 'buzz';
+    console.log(number);
     }
-    if(i%3 == 0 && i%5 == 0){
-        number = 'fizzbuzz';
-        console.log(number);
 
+    else{
+        console.log(i);
     }
 }
-
-
-
-
-// prendo il documento 
-// faccio la lista di numeri da 1 a 100
-// verifico se un numero è divisibile per 3
-// verifico se un numero è divisibile per 5
-// verifico se un numero è divisibile per entrambi 
